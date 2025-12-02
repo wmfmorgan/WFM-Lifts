d05ae07911857dcae83f5003b57eb192e9a65795
wmfmorgan <wfmorgan73@gmail.com>
2025-12-02 06:57:53 -0600

    updated run.py

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ app = create_app()
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User}
+     return {'db': db, 'User': User}
 
 if __name__ == '__main__':
     app.run(debug=True)
